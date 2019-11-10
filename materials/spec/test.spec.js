d5f9830a8d5f20e9f895dcd0c2d8fc03ec91aa99
Katy Gibson <klgibson528@gmail.com>
2019-11-10 17:43:09 -0600

    Started working on tests but am unable to run them.

--- materials/spec/test.spec.js
+++ materials/spec/test.spec.js
@@ -1,4 +1,19 @@
-describe("hello world", function(){
-    it("hello test", function(){
-    })
-})
+$(function () {
+    describe("recommendations", function () {
+        it("removes duplicates", function () {
+            expect(data).toBeDefined();
+            expect(data.length).not.toBe(0);
+        });
+    });
+
+    describe("fooltracker", function () {
+        it("creates an instance of the class FoolTracker", function () {
+            expect('fool').toBeInstanceOf('FoolTracker');
+        });
+    });
+
+
+});
+
+// This is as far as I got without being able to run the tests to see what was working. 
+// Wanted to test each function individually
